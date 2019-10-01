3ad416d2df3767e018d13fed7f947b2adc809b7b
wangke1996 <porkw@outlook.com>
2019-10-01 13:20:52 +0800

    Updates

---
+++ precache-manifest.0db3a1fdd1538932a76e4fbbb864964d.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d03f34b7066dc338616b8d79f8dfd0",
+    "url": "/homepage/index.html"
+  },
+  {
+    "revision": "ca5a1b7666752aa38e8a",
+    "url": "/homepage/static/css/2.ba46077e.chunk.css"
+  },
+  {
+    "revision": "0510e1f4f02bf7fb73c2",
+    "url": "/homepage/static/css/main.2f032a15.chunk.css"
+  },
+  {
+    "revision": "ca5a1b7666752aa38e8a",
+    "url": "/homepage/static/js/2.094921cb.chunk.js"
+  },
+  {
+    "revision": "0510e1f4f02bf7fb73c2",
+    "url": "/homepage/static/js/main.d7450098.chunk.js"
+  },
+  {
+    "revision": "d95e9343b73043b8e268",
+    "url": "/homepage/static/js/runtime-main.4ba72617.js"
+  },
+  {
+    "revision": "ebeacdd070049dcc75525c2607e97d14",
+    "url": "/homepage/static/media/Selfie.ebeacdd0.png"
+  },
+  {
+    "revision": "cb484c46bb2c71f8e207204cd04680a3",
+    "url": "/homepage/static/media/poster-SPD-SRU.cb484c46.png"
+  },
+  {
+    "revision": "0cb8863d749c0cbe25b0e98e7747fe49",
+    "url": "/homepage/static/media/poster.0cb8863d.png"
+  }
+]);
